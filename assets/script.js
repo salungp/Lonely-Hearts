1df5254f7eb9fa2335e0ed259722cd16ee498d79
Salung Prastyo <salungprastyo@Salungs-MacBook-Pro.local>
2025-08-02 02:04:10 +0700

    add nav dropdown javascript to all pages

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,3 @@
-const hamburger = document.getElementById('lhHamburger');
-    const menu = document.getElementById('lhMobileMenu');
-
-    // Toggle menu
-    hamburger.addEventListener('click', (e) => {
-      e.stopPropagation(); // Prevent it from triggering the document click
-      menu.classList.toggle('active');
-    });
-
-    // Close when clicking outside
-    document.addEventListener('click', (e) => {
-      if (!menu.contains(e.target) && !hamburger.contains(e.target)) {
-        menu.classList.remove('active');
-      }
-});
-
 class BootstrapPinInput {
     constructor() {
         this.inputs = document.querySelectorAll('.pin-input-field');
